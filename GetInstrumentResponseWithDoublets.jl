13317b9e3145a9b6ecdfec761cf0731d390304c1
thomasandrewlee <thomaslee@princeton.edu>
2024-10-01 15:35:48 -0400

    dljkjsdnfsdjkn Please enter the commit message for your changes. Lines starting

--- GetInstrumentResponseWithDoublets.jl
+++ GetInstrumentResponseWithDoublets.jl
@@ -44,10 +44,12 @@ c_new_ISC = string(usr_str,"Research/FindEQDoublets/ISC_M6_1988_2024.txt")
 # HRV data files
 c_HRV_old = string(usr_str,"Downloads/HRV_SAC_ANALOG/LPZ/")
 c_HRV_save = string(c_dataout,"HRV_1936_1940_LPZ.jld")
+# EQ save files
+c_oldEQ_save = string(c_dataout,"HRV_1936_1940_LPZ_oldEQ.jld")
 # search parameters
 surfvel = 3.0 # surface wave velocity km/s
-windowstart = -Dates.Minute(20)
-windowend = Dates.Minute(60) # window for surface waves
+windowstart = -Dates.Minute(05)
+windowend = Dates.Minute(55) # window for surface waves
 datathresh = 0.9 # data coverage required in window
 hrv_lat = 42.5060
 hrv_lon = 71.5580
@@ -110,90 +112,128 @@ else # read from sac
     save(c_HRV_save,"oldT",oldT,"oldD",oldD,"oldsamprate",samprate)
 end
 
-## LOAD ISC CATALOG FOR ANALOG
-# read in the ISC file
-ln = open(c_old_ISC) do f
-    readlines(f)
-end
-oldEQtme = []
-oldEQlat = []
-oldEQlon = []
-oldEQdep = []
-oldEQmag = []
-for il = 20:lastindex(ln) # skip header line
-    #print(string(il,"\n"))
-    commas = findall(map(x->ln[il][x]==',',1:lastindex(ln[il])))
-    # try read with subseconds
-    hypotime = tryparse(DateTime,ln[il][commas[3]+1:commas[5]-1],dateformat"y-m-d,H:M:S")
-    # if not, try without
-    if isnothing(hypotime)
-        hypotime = tryparse(DateTime,ln[il][commas[3]+1:commas[5]-4],dateformat"y-m-d,H:M:S")
+if isfile(c_oldEQ_save)
+    tmpvar = load(c_oldEQ_save)
+    oldEQtme = tmpvar["oldEQtme"]
+    oldEQlat = tmpvar["oldEQlat"]
+    oldEQlon = tmpvar["oldEQlon"]
+    oldEQdep = tmpvar["oldEQdep"]
+    oldEQmag = tmpvar["oldEQmag"]
+    oldEQtrace = tmpvar["oldEQtrace"]
+    oldEQspect = tmpvar["oldEQspect"]
+    oldEQspectF = tmpvar["oldEQspectF"]
+    tmpvar = []
+else
+    ## LOAD ISC CATALOG FOR ANALOG
+    # read in the ISC file
+    ln = open(c_old_ISC) do f
+        readlines(f)
     end
-    push!(oldEQtme,hypotime)
-    push!(oldEQlat,parse(Float64,ln[il][commas[5]+1:commas[6]-1]))
-    push!(oldEQlon,parse(Float64,ln[il][commas[6]+1:commas[7]-1]))
-    push!(oldEQdep,parse(Float64,ln[il][commas[7]+1:commas[8]-1]))
-    push!(oldEQmag,parse(Float64,ln[il][98:101]))
-end 
-print(string("Read ",length(oldEQtme)," events from ",c_old_ISC,"...\n"))
+    oldEQtme = []
+    oldEQlat = []
+    oldEQlon = []
+    oldEQdep = []
+    oldEQmag = []
+    for il = 20:lastindex(ln) # skip header line
+        #print(string(il,"\n"))
+        commas = findall(map(x->ln[il][x]==',',1:lastindex(ln[il])))
+        # try read with subseconds
+        hypotime = tryparse(DateTime,ln[il][commas[3]+1:commas[5]-1],dateformat"y-m-d,H:M:S")
+        # if not, try without
+        if isnothing(hypotime)
+            hypotime = tryparse(DateTime,ln[il][commas[3]+1:commas[5]-4],dateformat"y-m-d,H:M:S")
+        end
+        push!(oldEQtme,hypotime)
+        push!(oldEQlat,parse(Float64,ln[il][commas[5]+1:commas[6]-1]))
+        push!(oldEQlon,parse(Float64,ln[il][commas[6]+1:commas[7]-1]))
+        push!(oldEQdep,parse(Float64,ln[il][commas[7]+1:commas[8]-1]))
+        push!(oldEQmag,parse(Float64,ln[il][98:101]))
+    end 
+    print(string("Read ",length(oldEQtme)," events from ",c_old_ISC,"...\n"))
 
-## FIND ANALOG EVENTS COVERED
-oldEQtrace = []
-oldEQspect = []
-oldEQspectF = []
-# setup geodesic
-Ga, Gf = Geodesics.EARTH_R_MAJOR_WGS84, Geodesics.F_WGS84
-print("Finding events with data and calculating PSDs... \n")
-if !isdir(string(c_dataout,"oldevents/"))
-    mkdir(string(c_dataout,"oldevents/"))
-end
-for i in ProgressBar(1:lastindex(oldEQtme))
-    # get distance
-    dtmp = Geodesics.surface_distance(hrv_lon,hrv_lat,oldEQlon[i],oldEQlat[i],Ga)
-    dtmp = dtmp/1000 # convert to km from m
-    # estimate travel time
-    ttime = dtmp/surfvel # seconds
-    ttime = convert(Int,round(ttime*1000)) # milliseconds
-    # get estimate surface wave arrival window
-    stime = oldEQtme[i] + Dates.Millisecond(ttime) + windowstart
-    etime = oldEQtme[i] + Dates.Millisecond(ttime) + windowend
-    targidx = findall(stime .<= oldT .<= etime)
-    # check for data coverage 
-    if sum(.!isnan.(oldD[targidx]))/length(targidx) >= datathresh # data is there
-        # save waveform
-        push!(oldEQtrace,oldD[targidx])
-        # calculate fft
-        noNaNtrace = oldD[targidx]
-        noNaNtrace[isnan.(noNaNtrace)] .= mean(filter(!isnan,noNaNtrace))
-        specttmpD = FFTW.rfft(noNaNtrace)
-        specttmpF = FFTW.rfftfreq(length(targidx),1/(Dates.value(oldsamprate)/1000))
-        # convert to PSD
-        specttmpPSD = 2*(1/((Dates.value(oldsamprate)/1000)*length(targidx))).*(abs.(specttmpD).^2)
-        # save PSD
-        push!(oldEQspect,specttmpPSD)
-        push!(oldEQspectF,specttmpF)
-        # plot and write out waveform and spectras
-        hpw = plot(oldT[targidx],oldD[targidx],lc=:black,legend=false,ylabel="pixels",
-            title=string(Dates.format(oldEQtme[i],"yyyy-mm-ddTHH:MM:SS.sss"),
-                "; M",oldEQmag[i],"; (",oldEQlat[i],", ",oldEQlon[i],
-                "); ",round(dtmp),"km -> ",ttime/1000,"s"))
-        hps = plot(1 ./specttmpF[2:end],specttmpPSD[2:end],xaxis=:log,yaxis=:log,lc=:black,
-            label="raw",xlabel="Period (s)",ylabel="pixels^2/Hz",minorgrid=true) 
-        plot!(hps,1 ./specttmpF[2:end],movmean(specttmpPSD[2:end],50),lc=:red,label="smoothed")
-            # smoothing is roughly a 1Hz window
-        hpall = plot(hpw,hps,layout=grid(2,1),size=(1000,1000))
-        savefig(hpall,string(c_dataout,"oldevents/",Dates.format(oldEQtme[i],"yyyymmddTHHMMSS"),".pdf"))
-    else # if there is no data
-        # fill with NaN for the new fields to go back and delete later
-        push!(oldEQtrace,[NaN])
-        push!(oldEQspect,[NaN])
-        push!(oldEQspectF,[NaN])
+    ## FIND ANALOG EVENTS COVERED
+    oldEQtrace = []
+    oldEQspect = []
+    oldEQspectF = []
+    # setup geodesic
+    Ga, Gf = Geodesics.EARTH_R_MAJOR_WGS84, Geodesics.F_WGS84
+    print("Finding events with data and calculating PSDs... \n")
+    if !isdir(string(c_dataout,"oldevents/"))
+        mkdir(string(c_dataout,"oldevents/"))
     end
-end
-# delete the dataless events
+    for i in ProgressBar(1:lastindex(oldEQtme))
+        # get distance
+        dtmp = Geodesics.surface_distance(hrv_lon,hrv_lat,oldEQlon[i],oldEQlat[i],Ga)
+        dtmp = dtmp/1000 # convert to km from m
+        # estimate travel time
+        ttime = dtmp/surfvel # seconds
+        ttime = convert(Int,round(ttime*1000)) # milliseconds
+        # get estimate surface wave arrival window
+        stime = oldEQtme[i] + Dates.Millisecond(ttime) + windowstart
+        etime = oldEQtme[i] + Dates.Millisecond(ttime) + windowend
+        targidx = findall(stime .<= oldT .<= etime)
+        # check for data coverage 
+        if sum(.!isnan.(oldD[targidx]))/length(targidx) >= datathresh # data is there
+            # subtract non-paramteric trend
+            tracetmp = oldD[targidx]
+            tracetmp = tracetmp .- movmean(tracetmp,convert(Int,round(length(targidx)/10)))
+            # calculate fft
+            noNaNtrace = oldD[targidx]
+            noNaNtrace[isnan.(noNaNtrace)] .= mean(filter(!isnan,noNaNtrace))
+            specttmpD = FFTW.rfft(noNaNtrace)
+            specttmpF = FFTW.rfftfreq(length(targidx),1/(Dates.value(oldsamprate)/1000))
+            # convert to PSD
+            specttmpPSD = 2*(1/((Dates.value(oldsamprate)/1000)*length(targidx))).*(abs.(specttmpD).^2)
+            # plot and write out waveform and spectras
+            hpw = plot(oldT[targidx],tracetmp,lc=:black,legend=false,ylabel="pixels",
+                title=string(Dates.format(oldEQtme[i],"yyyy-mm-ddTHH:MM:SS.sss"),
+                    "; M",oldEQmag[i],"; (",oldEQlat[i],", ",oldEQlon[i],", ",oldEQdep[i],
+                    "); ",round(dtmp),"km -> ",ttime/1000,"s"))
+            hps = plot(1 ./specttmpF[2:end],specttmpPSD[2:end],xaxis=:log,yaxis=:log,lc=:black,
+                label="raw",xlabel="Period (s)",ylabel="pixels^2/Hz",minorgrid=true) 
+            plot!(hps,1 ./specttmpF[2:end],movmean(specttmpPSD[2:end],50),lc=:red,label="smoothed")
+                # smoothing is roughly a 1Hz window
+            hpall = plot(hpw,hps,layout=grid(2,1),size=(1000,1000))
+            display(hpall)
+            # check with user if data is appropro
+            print("\nUse this event?\n\n")
+            usedata = readline()
+            if usedata
+                # save plot
+                savefig(hpall,string(c_dataout,"oldevents/",Dates.format(oldEQtme[i],"yyyymmddTHHMMSS"),".pdf"))
+                # save PSD and trace
+                push!(oldEQspect,specttmpPSD)
+                push!(oldEQspectF,specttmpF)
+                push!(oldEQtrace,tracetmp)
+            else
+                # fill with NaN for the new fields
+                push!(oldEQtrace,[NaN])
+                push!(oldEQspect,[NaN])
+                push!(oldEQspectF,[NaN])
+            end
+        else # if there is no data
+            # fill with NaN for the new fields to go back and delete later
+            push!(oldEQtrace,[NaN])
+            push!(oldEQspect,[NaN])
+            push!(oldEQspectF,[NaN])
+        end
+    end
+    # delete the dataless events
 
-# report
-print(string("Found ",length(oldEQtme)," events for historical HRV...\n"))
+    # save
+    save(c_oldEQ_save,
+        "oldEQtme",oldEQtme,
+        "oldEQlat",oldEQlat,
+        "oldEQlon",oldEQlon,
+        "oldEQdep",oldEQdep,
+        "oldEQmag",oldEQmag,
+        "oldEQtrace",oldEQtrace,
+        "oldEQspect",oldEQspect,
+        "oldEQspectF",oldEQspectF,
+    )
+    # report
+    print(string("Found ",length(oldEQtme)," events for historical HRV...\n"))
+end
 
 ## LOAD MODERN ISC CATALOG
 
